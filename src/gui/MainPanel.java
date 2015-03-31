e3d9a8c55fb38725d86fa2a49ab8d8ecdf10f967
stijn26 <stijnaerts32@gmail.com>
2015-03-31 20:24:44 +0200

    Kleine aanpassing

--- src/gui/MainPanel.java
+++ src/gui/MainPanel.java
@@ -105,35 +105,5 @@ public class MainPanel extends GridPane {
         });
     }
 
-//    private static class MyNode {
-//
-//        String value;
-//        String type;
-//        int id;
-//
-//        public MyNode(String value, String type, int id) {
-//            this.type = type;
-//            this.value = value;
-//            this.id = id;
-//        }
-//
-//        public boolean isContinent() {
-//            return type.equalsIgnoreCase("Continent");
-//        }
-//
-//        public boolean isCountry() {
-//            return type.equalsIgnoreCase("Country");
-//        }
-//
-//        public boolean isClimateChart() {
-//            return type.equalsIgnoreCase("ClimateChart");
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return value;
-//
-//        }
-//    }
 
 }
