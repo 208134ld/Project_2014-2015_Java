c558c37760c8ef799bbb0cd4f0d96ce47c7b4c7c
stijn26 <stijnaerts32@gmail.com>
2015-03-31 15:11:26 +0200

    TreeView opgevuld met objecten ipv strings

--- src/gui/MainPanel.java
+++ src/gui/MainPanel.java
@@ -8,17 +8,22 @@ package gui;
 import domain.ClimateChart;
 import domain.Continent;
 import domain.Country;
+import domain.TextFieldTreeCellImpl;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 import persistentie.ContinentRepository;
 
 /**
@@ -30,12 +35,12 @@ public class MainPanel extends GridPane {
 
     @FXML
     TreeView selectionTreeView;
-    
+
     private ContinentRepository continentRepository;
 
-    public MainPanel() throws SQLException{
+    public MainPanel() throws SQLException {
         continentRepository = new ContinentRepository();
-        
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPanel.fxml"));
         loader.setRoot(this);
         loader.setController(this);
@@ -44,18 +49,17 @@ public class MainPanel extends GridPane {
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
-        
-        TreeItem<String> root = new TreeItem<>("Root");
-        
-        for(Continent c : continentRepository.getAllContinents()){
-            TreeItem<String> itemChild = new TreeItem<>(c.getName());
 
+        TreeItem<MyNode> root = new TreeItem<>(new MyNode("Root node", "Root", 1));
+
+        for(Continent c : continentRepository.getAllContinents()){
+            TreeItem<MyNode> itemChild = new TreeItem<>(new MyNode(c.getName(), "Continent", c.getId()));
+            
             for(Country co : continentRepository.getCountriesOfContinent(c.getId())){
-                TreeItem<String> countryChild = new TreeItem<>(co.getName());
-                //List<ClimateChart> list = continentRepository.getClimateChartsOfCountry(co.getId());
-                //int i = 0;
+                TreeItem<MyNode> countryChild = new TreeItem<>(new MyNode(co.getName(), "Country", co.getId()));
+
                 for(ClimateChart chart : continentRepository.getClimateChartsOfCountry(co.getId())){
-                    TreeItem<String> climateChartChild = new TreeItem<>(chart.getLocation());
+                    TreeItem<MyNode> climateChartChild = new TreeItem<>(new MyNode(chart.getLocation(), "ClimateChart", chart.getId()));
                     countryChild.getChildren().add(climateChartChild);
                 }
                 
@@ -64,13 +68,59 @@ public class MainPanel extends GridPane {
             
             root.getChildren().add(itemChild);
         }
-        
         root.setExpanded(true);
+
+        //Onderstaand gedeelte maakt het mogelijk om treeviewitems "on the spot" van naam te veranderen, dit werkt alleen met treeItem<String> dus moet nog aangepast worden
+//        selectionTreeView.setEditable(true);
+//        selectionTreeView.setCellFactory(new Callback<TreeView<String>,TreeCell<String>>(){
+//            @Override
+//            public TreeCell<String> call(TreeView<String> p) {
+//                TextFieldTreeCellImpl textField = null;
+//                try {
+//                    textField = new TextFieldTreeCellImpl();
+//                } catch (SQLException ex) {
+//                    Logger.getLogger(MainPanel.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//                return textField;
+//            }
+//
+//            
+//        });
         
         //itemChild.setExpanded(false);
-        
-        
         selectionTreeView.setRoot(root);
         selectionTreeView.setShowRoot(false);
     }
+
+    private static class MyNode {
+
+        String value;
+        String type;
+        int id;
+
+        public MyNode(String value, String type, int id) {
+            this.type = type;
+            this.value = value;
+            this.id = id;
+        }
+
+        public boolean isContinent() {
+            return type.equalsIgnoreCase("Continent");
+        }
+
+        public boolean isCountry() {
+            return type.equalsIgnoreCase("Country");
+        }
+
+        public boolean isClimateChart() {
+            return type.equalsIgnoreCase("ClimateChart");
+        }
+
+        @Override
+        public String toString() {
+            return value;
+
+        }
+    }
+
 }
