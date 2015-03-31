599deebce4ceb1cec3df9ed55f79f45ed42a37c0
yoloswagings <arnedebremme@hotmail.com>
2015-03-31 13:01:21 +0200

    climatechart data toegevoegd

--- src/domain/ClimateChart.java
+++ src/domain/ClimateChart.java
@@ -6,6 +6,7 @@
 package domain;
 
 import javafx.beans.property.*;
+import static javafx.beans.property.DoubleProperty.doubleProperty;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -27,11 +28,60 @@ public class ClimateChart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ClimateChartID")
-    private IntegerProperty climateChartId;
+    private int climateChartId;
     @ManyToOne
     private Country country;
-
+    private IntegerProperty beginperiod, endperiod;
+    private int[] sedimentArray,TempArray;
+    private boolean aboveEquator;
+    private DoubleProperty latidude,longitude;
     public ClimateChart(){}
+
+    public void setClimateChartId(int climateChartId) {
+        this.climateChartId = climateChartId;
+    }
+    
+    public void setBeginperiod(int b)
+    {
+        this.beginperiod.set(b);
+    }
+    public  int getBeginperiod()
+    {
+        return this.beginperiod.get();
+    }
+    public void setEndperiod(int e)
+    {
+        this.endperiod.set(e);
+    }
+    public  int getEndperiod()
+    {
+        return this.endperiod.get();
+    }
+
+    public int[] getSedimentArray() {
+        return sedimentArray;
+    }
+
+    public void setSedimentArray(int[] sedimentArray) {
+        this.sedimentArray = sedimentArray;
+    }
+
+    public int[] getTempArray() {
+        return TempArray;
+    }
+
+    public void setTempArray(int[] TempArray) {
+        this.TempArray = TempArray;
+    }
+
+    public boolean isAboveEquator() {
+        return aboveEquator;
+    }
+
+    public void setAboveEquator(boolean aboveEquator) {
+        this.aboveEquator = aboveEquator;
+    }
+    
     public Country getCountry() {
         return country;
     }
@@ -43,24 +93,48 @@ public class ClimateChart {
     public void ClimateChart(String location)
     {
         this.location = new SimpleStringProperty(location);
-        climateChartId = new SimpleIntegerProperty();
+       
         
     }
-
+    public void ClimateChart(String loc,int begin,int end,int[]temp,int[]sed,double latidude,double longitude,int id)
+    {
+        this.location = new SimpleStringProperty(loc);
+        setClimateChartId(id);
+        this.endperiod = new SimpleIntegerProperty(end); 
+        this.beginperiod = new SimpleIntegerProperty(begin);
+        setTempArray(temp);
+        setSedimentArray(sed);
+        this.latidude = new SimpleDoubleProperty(latidude);
+        this.longitude = new SimpleDoubleProperty(longitude);
+        
+    }
     public StringProperty getLocationProp() {
         return location;
     }
 
-    public IntegerProperty getClimateChartIdProp() {
-        return climateChartId;
-    }
 
     public String getLocation() {
         return location.get();
     }
 
     public Integer getId() {
-        return climateChartId.get();
+        return climateChartId;
+    }
+
+    public double getLatidude() {
+        return latidude.get();
+    }
+
+    public void setLatidude(double latidude) {
+        this.latidude.set(latidude);
+    }
+
+    public double getLongitude() {
+        return longitude.get();
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude.set(longitude);
     }
 
     
