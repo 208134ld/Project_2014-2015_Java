3b85a3e586e64549da6013c0d8d38b773efde92f
SamuelCaudenberg <scaudenberg@hotmail.com>
2015-04-08 17:16:51 +0200

    Klasse toevoegen

---
+++ src/domain/Grade.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author SAMUEL
+ */
+@Entity(name="Grades")
+@Table(name = "Grades")
+public class Grade implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "GradeId")
+    private int grade;
+    
+    @OneToMany(mappedBy = "grade")
+    private List<SchoolYear> schoolYears;
+
+    //CTOR
+    public Grade() {
+    }
+
+    public Grade(int grade) {
+        this.grade = grade;
+    }
+    
+    //GET SET
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public List<SchoolYear> getSchoolYears() {
+        return schoolYears;
+    }
+
+    public void setSchoolYears(List<SchoolYear> schoolYears) {
+        this.schoolYears = schoolYears;
+    }
+    
+    
+    
+}
