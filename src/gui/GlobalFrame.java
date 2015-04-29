d0644531d1c129bb01aebe52cc846d53c3168771
SamuelCaudenberg <scaudenberg@hotmail.com>
2015-04-29 17:40:50 +0200

    EventHandler toegevoegd aan menu van GlobalFrame

--- src/gui/GlobalFrame.java
+++ src/gui/GlobalFrame.java
@@ -1,27 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import repository.RepositoryController;
 
-/**
- * FXML Controller class
- *
- * @author Logan Dupont
- */
 public class GlobalFrame extends VBox {
 
     @FXML
     private MenuBar mainMenu;
+
+    @FXML
+    private Menu mManage;
+
+    @FXML
+    private MenuItem miDeterminateTabel;
+
+    @FXML
+    private MenuItem miClassList;
+
+    @FXML
+    private MenuItem miClimateChart;
+
+    @FXML
+    private Menu mOverview;
+
+    @FXML
+    private MenuItem miStatistics;
+
+    @FXML
+    private MenuItem miWeb;
 
     @FXML
     private GridPane workPanel;
@@ -30,13 +45,16 @@ public class GlobalFrame extends VBox {
 
     private final LocationControllerPanel locationControllerPanel;
     private final LocationViewPanel locationViewPanel;
+    private final ClassListControllerPanel classListControllerPanel;
+    private final ClassListViewPanel classListViewPanel;
 
     public GlobalFrame() {
 
         this.repositoryController = new RepositoryController();
         locationControllerPanel = new LocationControllerPanel(repositoryController);
-
         locationViewPanel = new LocationViewPanel(repositoryController);
+        classListControllerPanel = new ClassListControllerPanel();
+        classListViewPanel = new ClassListViewPanel();
         //repositoryController.addObserver(mainPanel);
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("GlobalFrame.fxml"));
@@ -50,6 +68,16 @@ public class GlobalFrame extends VBox {
 
         workPanel.add(locationControllerPanel, 0, 0);
         workPanel.add(locationViewPanel, 1, 0);
+
+        //KLIK OP KLASLIJST
+        miClassList.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                workPanel.getChildren().clear();
+                workPanel.add(classListControllerPanel, 0, 0);
+                workPanel.add(classListViewPanel, 1, 0);
+            }
+        });
 
     }
 
